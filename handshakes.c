d16499701ff912f45643cd1ed2aa8e3a21c77b57
kps004 <36299014+kps004@users.noreply.github.com>
2019-07-26 14:02:31 +0530

    Create handshakes.c

---
+++ handshakes.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int i,j,k,l;
+   scanf("%d",&k);
+   i= (k*(k-1)/2);
+   printf("%d",i);
+   
+  
+    return 0;
+}
